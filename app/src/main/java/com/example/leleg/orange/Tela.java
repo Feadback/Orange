2570969c2c0dd1b840b0ac8f5422742245ddc3c4
LeticiaLCavalcanti <wmli515@gmail.com>
2018-04-12 01:14:33 -0300

    Initial commit

---
+++ app/src/main/java/com/example/leleg/orange/Tela.java
@@ -0,0 +1,13 @@
+package com.example.leleg.orange;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Tela extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout);
+    }
+}
